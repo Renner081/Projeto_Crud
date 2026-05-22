58fabaa6dbf7cbabc686dbff4ea856bd4a040c0c
Renner081 <Renner.luis123@gmail.com>
2026-05-22 18:22:49 -0300

    Data base atlz

--- My_Crud/Database/my_crud.sql
+++ My_Crud/Database/my_crud.sql
@@ -35,6 +35,16 @@ CREATE TABLE `produtos` (
   `preco` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Índices para tabelas despejadas
 --
